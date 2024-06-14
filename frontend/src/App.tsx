ca524eb7a6807838cd8631419f525181a59ea333
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-14 11:41:19 +0530

    Project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import EventForm from "./components/EventForm";
+
+function App() {
+    return (
+        <div>
+            <h1>Create Event</h1>
+            <EventForm />
+        </div>
+    );
+}
+
+export default App;
