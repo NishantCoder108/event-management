73a39d9560548d9bdf9819bd49fe1bf25c425a88
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-16 15:26:09 +0530

    Implement create event logic

--- backend/src/controllers.ts
+++ backend/src/controllers.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express";
-// import { saveEvent } from "./models";
+import pool from "./config/db";
+import { createEventTableQuery } from "./utils/eventTalbeQuery";
+import { checkAndCreateTable } from "./utils/tableUtils";
 
-export const createEvent = async (req: Request, res: Response) => {
+interface EventRequest extends Request {
+    body: {
+        eventName: string;
+        datetime: string;
+        duration: number;
+        location: string;
+        agenda: string;
+        guests: string[];
+        reminder: string;
+        notification: string;
+        attachment: string | File;
+    };
+}
+
+export const createEvent = async (req: EventRequest, res: Response) => {
     try {
         const eventData = req.body;
         const eventFile = req.file;
@@ -9,13 +25,54 @@ export const createEvent = async (req: Request, res: Response) => {
         if (eventFile) {
             eventData.attachment = eventFile.path;
         }
-        console.log({ eventData });
-        res.status(201).send({
-            eventData,
-            message: "Event created successfully",
-        });
 
-        // await saveEvent(eventData);
+        const {
+            eventName,
+            datetime,
+            duration,
+            location,
+            agenda,
+            guests,
+            reminder,
+            notification,
+            attachment,
+        } = eventData;
+
+        if (
+            !eventName ||
+            !datetime ||
+            !duration ||
+            !location ||
+            !agenda ||
+            !guests ||
+            !reminder ||
+            !notification ||
+            !attachment
+        ) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        await checkAndCreateTable("events", createEventTableQuery);
+
+        const query = `
+        INSERT INTO events (eventName, datetime, duration, location, agenda, guests, reminder, notification, attachment)
+        VALUES (?, FROM_UNIXTIME(? / 1000), ?, ?, ?, ?, ?, ?, ?)
+      `;
+
+        const [results] = await pool.execute(query, [
+            eventName,
+            datetime,
+            duration,
+            location,
+            agenda,
+            guests,
+            reminder,
+            notification,
+            attachment,
+        ]);
+
+        console.log("Event saved:", results);
+        res.status(201).json({ message: "Event created successfully" });
     } catch (error) {
         console.log("ERROR :", error);
         res.status(500).send({ message: "Error creating event", error: error });
