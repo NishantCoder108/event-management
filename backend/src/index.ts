34bcf135dbe65ed112ea50bcd88c490f68e63a09
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-17 15:10:22 +0530

    Serve frontend from backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,25 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
 import multer from "multer";
 import { eventRoutes } from "./routes";
+import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 const upload = multer({ dest: "uploads/" });
 
-app.use((req, res, next) => {
-    console.log("Request body: " + JSON.stringify(req.body, null, 2));
-    const file = req.file;
-    console.log("File: ", file, JSON.stringify(file, null, 2));
-    next();
-});
 app.use("/api/events", upload.single("attachment"), eventRoutes);
 
-app.get("/test", (req, res) => {
-    res.send("Hello World");
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
