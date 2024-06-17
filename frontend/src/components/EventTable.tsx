e1d1fc68d26aef0c86529aefc29f905fc058e30a
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-18 03:50:40 +0530

    UI improvement of Table

--- frontend/src/components/EventTable.tsx
+++ frontend/src/components/EventTable.tsx
@@ -15,9 +15,11 @@ import {
     Box,
     Grid,
     Typography,
+    Link,
 } from "@mui/material";
 import { format } from "date-fns";
 import axiosInstance from "../api/axiosInstance";
+import { formatTime } from "../utils/formatTime";
 
 interface Event {
     id: number;
@@ -34,6 +36,10 @@ interface Event {
 interface EventTableProps {
     refreshData: boolean;
 }
+
+interface ISortColumn {
+    id: number;
+}
 const EventTable = memo(({ refreshData }: EventTableProps) => {
     const [events, setEvents] = useState<Event[]>([]);
     const [status, setStatus] = useState<string>("");
@@ -47,7 +53,15 @@ const EventTable = memo(({ refreshData }: EventTableProps) => {
                     duration: duration || undefined,
                 },
             });
-            setEvents(response.data);
+            console.log("Fetched Events", response.data);
+
+            if (response && response.data) {
+                const sortedData = response.data.sort(
+                    (a: ISortColumn, b: ISortColumn) => b.id - a.id
+                );
+                console.log("Sorted Events", sortedData);
+                setEvents(sortedData);
+            }
         } catch (error) {
             console.error("Error fetching events:", error);
         }
@@ -61,8 +75,33 @@ const EventTable = memo(({ refreshData }: EventTableProps) => {
         <Box sx={{ padding: 2 }}>
             <Grid container spacing={2} alignItems="center">
                 <Grid item>
-                    <FormControl variant="outlined" sx={{ minWidth: 120 }}>
-                        <InputLabel>Status</InputLabel>
+                    <FormControl
+                        variant="outlined"
+                        size="small"
+                        sx={{
+                            minWidth: 110,
+                            // height: "1rem",
+                            // border: "1px solid red",
+
+                            "& .MuiSvgIcon-root": {
+                                fontSize: "large",
+                            },
+                            "& .MuiInputBase-root": {
+                                fontSize: "small",
+                            },
+
+                            "& .MuiInputBase-input .MuiSelect-select": {
+                                padding: ".4rem",
+                                border: "1px solid red",
+                                fontSize: "small",
+                            },
+                        }}
+                    >
+                        <InputLabel
+                            sx={{ fontSize: "small", fontWeight: "bold" }}
+                        >
+                            Status
+                        </InputLabel>
                         <Select
                             value={status}
                             onChange={(e) =>
@@ -70,18 +109,58 @@ const EventTable = memo(({ refreshData }: EventTableProps) => {
                             }
                             label="Status"
                         >
-                            <MenuItem value="">
+                            <MenuItem value="" sx={{ fontSize: "small" }}>
                                 <em>None</em>
                             </MenuItem>
-                            <MenuItem value="completed">Completed</MenuItem>
-                            <MenuItem value="ongoing">Ongoing</MenuItem>
-                            <MenuItem value="upcoming">Upcoming</MenuItem>
+                            <MenuItem
+                                value="completed"
+                                sx={{ fontSize: "small" }}
+                            >
+                                Completed
+                            </MenuItem>
+                            <MenuItem
+                                value="ongoing"
+                                sx={{ fontSize: "small" }}
+                            >
+                                Ongoing
+                            </MenuItem>
+                            <MenuItem
+                                value="upcoming"
+                                sx={{ fontSize: "small" }}
+                            >
+                                Upcoming
+                            </MenuItem>
                         </Select>
                     </FormControl>
                 </Grid>
                 <Grid item>
-                    <FormControl variant="outlined" sx={{ minWidth: 120 }}>
-                        <InputLabel>Duration</InputLabel>
+                    <FormControl
+                        size="small"
+                        variant="outlined"
+                        sx={{
+                            minWidth: 100,
+                            // height: "1rem",
+                            // border: "1px solid red",
+
+                            "& .MuiSvgIcon-root": {
+                                fontSize: "large",
+                            },
+                            "& .MuiInputBase-root": {
+                                fontSize: "small",
+                            },
+
+                            "& .MuiInputBase-input .MuiSelect-select": {
+                                padding: ".4rem",
+                                border: "1px solid red",
+                                fontSize: "small",
+                            },
+                        }}
+                    >
+                        <InputLabel
+                            sx={{ fontSize: "small", fontWeight: "bold" }}
+                        >
+                            Duration
+                        </InputLabel>
                         <Select
                             value={duration}
                             onChange={(e) =>
@@ -89,22 +168,66 @@ const EventTable = memo(({ refreshData }: EventTableProps) => {
                             }
                             label="Duration"
                         >
-                            <MenuItem value="">
+                            <MenuItem sx={{ fontSize: "small" }} value="">
                                 <em>None</em>
                             </MenuItem>
-                            <MenuItem value="1800000">30 minutes</MenuItem>
-                            <MenuItem value="3600000">1 hour</MenuItem>
-                            <MenuItem value="5400000">1.5 hours</MenuItem>
-                            <MenuItem value="7200000">2 hours</MenuItem>
-                            <MenuItem value="9000000">2.5 hours</MenuItem>
-                            <MenuItem value="10800000">3 hours</MenuItem>
-                            <MenuItem value="14400000">4 hours</MenuItem>
-                            <MenuItem value="18000000">5 hours</MenuItem>
+                            <MenuItem
+                                value="1800000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                30 minutes
+                            </MenuItem>
+                            <MenuItem
+                                value="3600000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                1 hour
+                            </MenuItem>
+                            <MenuItem
+                                value="5400000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                1.5 hours
+                            </MenuItem>
+                            <MenuItem
+                                value="7200000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                2 hours
+                            </MenuItem>
+                            <MenuItem
+                                value="9000000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                2.5 hours
+                            </MenuItem>
+                            <MenuItem
+                                value="10800000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                3 hours
+                            </MenuItem>
+                            <MenuItem
+                                value="14400000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                4 hours
+                            </MenuItem>
+                            <MenuItem
+                                value="18000000"
+                                sx={{ fontSize: "small" }}
+                            >
+                                5 hours
+                            </MenuItem>
                         </Select>
                     </FormControl>
                 </Grid>
                 <Grid item>
-                    <Button variant="contained" onClick={fetchEvents}>
+                    <Button
+                        variant="contained"
+                        onClick={fetchEvents}
+                        sx={{ fontSize: "x-small", fontWeight: "bold" }}
+                    >
                         Filter
                     </Button>
                 </Grid>
@@ -113,22 +236,106 @@ const EventTable = memo(({ refreshData }: EventTableProps) => {
             <TableContainer component={Paper} sx={{ marginTop: 2 }}>
                 <Table size="small">
                     <TableHead>
-                        <TableRow>
-                            <TableCell>ID</TableCell>
-                            <TableCell>Event Name</TableCell>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Time</TableCell>
-                            <TableCell>Duration</TableCell>
-                            <TableCell>Location</TableCell>
-                            <TableCell>Agenda</TableCell>
-                            <TableCell>Guests</TableCell>
-                            <TableCell>Reminder</TableCell>
-                            <TableCell>Notification</TableCell>
-                            <TableCell>Attachment</TableCell>
+                        <TableRow sx={{ fontSize: "small" }}>
+                            <TableCell
+                                sx={{ fontSize: "small", fontWeight: "bold" }}
+                            >
+                                ID
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "10rem",
+                                }}
+                            >
+                                Event Name
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "5rem",
+                                }}
+                            >
+                                Date
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "4rem",
+                                }}
+                            >
+                                Time
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "4rem",
+                                }}
+                            >
+                                Duration
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "6rem",
+                                }}
+                            >
+                                Location
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "10rem",
+                                }}
+                            >
+                                Agenda
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "3rem",
+                                }}
+                            >
+                                Guests
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "5rem",
+                                }}
+                            >
+                                Reminder
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "3rem",
+                                }}
+                            >
+                                Notification
+                            </TableCell>
+                            <TableCell
+                                sx={{
+                                    fontSize: "small",
+                                    fontWeight: "bold",
+                                    minWidth: "6rem",
+                                }}
+                            >
+                                Attachment
+                            </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody sx={{ fontSize: "small " }}>
-                        {events.map((event) => (
+                        {events.map((event, idx) => (
                             <TableRow key={event.id}>
                                 <TableCell sx={{ fontSize: "small" }}>
                                     <Typography
@@ -136,30 +343,46 @@ const EventTable = memo(({ refreshData }: EventTableProps) => {
                                         component={"p"}
                                     >
                                         {" "}
-                                        {event.id}{" "}
+                                        {idx + 1}{" "}
                                     </Typography>
                                 </TableCell>
                                 <TableCell sx={{ fontSize: "small " }}>
                                     {event.eventName}
                                 </TableCell>
-                                <TableCell>
+                                <TableCell sx={{ fontSize: "small " }}>
                                     {format(event.datetime, "MMM d, yyyy")}
                                 </TableCell>
-                                <TableCell>
+                                <TableCell sx={{ fontSize: "small " }}>
                                     {format(event.datetime, "h:mm a")}{" "}
                                 </TableCell>
-                                <TableCell>
-                                    {(event.duration / (1000 * 60)).toFixed(2)}{" "}
-                                    min
+                                <TableCell sx={{ fontSize: "small " }}>
+                                    {formatTime(event.duration)}
                                 </TableCell>
-                                <TableCell>{event.location}</TableCell>
-                                <TableCell>{event.agenda}</TableCell>
-                                <TableCell>{event.guests.join(", ")}</TableCell>
-                                <TableCell>
-                                    {event.reminder} min before
+                                <TableCell sx={{ fontSize: "small " }}>
+                                    {event.location}
                                 </TableCell>
-                                <TableCell>{event.notification}</TableCell>
-                                <TableCell>{event.attachment}</TableCell>
+                                <TableCell sx={{ fontSize: "small " }}>
+                                    {event.agenda}
+                                </TableCell>
+                                <TableCell sx={{ fontSize: "small " }}>
+                                    {event.guests.join(", ")}
+                                </TableCell>
+                                <TableCell sx={{ fontSize: "small " }}>
+                                    {formatTime(event.reminder)} before
+                                </TableCell>
+                                <TableCell sx={{ fontSize: "small " }}>
+                                    {event.notification}
+                                </TableCell>
+                                <TableCell sx={{ fontSize: "small " }}>
+                                    <Link
+                                        href={event.attachment}
+                                        underline="hover"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        View File
+                                    </Link>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
